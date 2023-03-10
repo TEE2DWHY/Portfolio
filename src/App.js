06d92cae8c1d3e8062ed231b8a64de59e23a1a6c
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-03-10 01:47:51 +0100

    made changes to features page

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Navbar from './components/Navbar';
+import Features from './components/Features';
+import About from './components/About';
+import Work from './components/Work';
+import Project from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+// Aos Library
+import Aos from "aos"
+import "aos/dist/aos.css"
+
+function App() {
+
+  Aos.init({
+    duration: 2000
+  })
+
+  return (
+    <>
+      <Navbar />
+      <div data-aos="zoom-in"><Features /></div>
+      <div  data-aos="slide-up"><About /></div>
+      <div  data-aos="slide-up"><Work /></div>
+      <div  data-aos="slide-up"><Project /></div>
+      <div  data-aos="slide-up"><Contact /></div>
+      <div  data-aos="slide-up"><Footer /></div>
+    </>
+  );
+}
+
+export default App;
--- src/components/Features.js
+++ src/components/Features.js
@@ -6,14 +6,14 @@ function Features() {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-9 features">
-                        <p className="numeric-01">Hi🎈, my name is</p>
+                        <p className="numeric-01">Hi, my name is</p>
                         <h1 className="header-name">Olorunfemi Tayo.</h1>
                         <h3 className="h3-features">I build things for the web.</h3>
                         <p className="p-1">I’m a software engineer specializing in building (and occasionally designing)
                             exceptional digital experiences. Currently, I’m focused on building accessible, human-centered
-                            products with great user <a className="acryn-green"
-                                href="https://github.com/TEE2DWHY?tab=repositories">Experience.</a> </p><button
-                                    className="projects">check out my projects!</button>
+                            products with great user <span className="acryn-green">Experience.</span> </p>
+                                <a href="https://github.com/TEE2DWHY?tab=repositories"><button
+                                    className="projects">check out my projects!</button></a>
                     </div>
                 </div>
             </div>
